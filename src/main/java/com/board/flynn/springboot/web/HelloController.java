eb5bfab81df612e1314fabf09c44fdf2ad4a90a7
Flynn_io <kootae88@gmail.com>
2021-02-21 22:12:32 +0900

    헬로 컨트롤러 주석 확인 할 것. 2회독시 드릴 다운 시도할 것

--- src/main/java/com/board/flynn/springboot/web/HelloController.java
+++ src/main/java/com/board/flynn/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.board.flynn.springboot.web;
 
+import com.board.flynn.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,17 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+        //새 오브젝트로 HelloResponseDto에 매개 변수로 name과 amount 를 반환 했음.
+        //그런데 HelloResponseDto에는 매개변수를 받는 부분이 없음.
+        // 어노테이션을 쓸수 있게해준 Dependency들 덕분인가 ? 임포트 부분의 Lib or frameWork때문인가?
+        //그것도 아니라면 view단 엔드포인트에서 주는 값을 name,amount값을 할당문 일 것 같다.(),= 으로 할당문 처럼. - 유
+        // 생성자 관계가 아직 풀리지 않은 숙제. 우선은 빠르게 1회독하고 한번 더 볼때 임포트 파트와 어노테이션 부분을 이해 해야겠다.
+        //C++에선 rule of 0,3,5로 수동으로 제어했었는데, 지금의 자바스프링부트는 어떨까, 자바는 어떨까 더 서칭 필요.
     }
 }
--- src/test/java/com/board/flynn/springboot/web/HelloControllerTest.java
+++ src/test/java/com/board/flynn/springboot/web/HelloControllerTest.java
@@ -4,13 +4,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -28,4 +33,19 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @Test
+    public void helloDTO가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
+
+
